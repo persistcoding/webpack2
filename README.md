8a57480e015448ac5109778f00ef71302218e2a9
minnaq <persist.coding@gmail.com>
2017-03-15 08:46:30 +0800

    demo3 configuring-hmr

--- README.md
+++ README.md
@@ -3,27 +3,14 @@ starter for webpack2
 
 # remark
 
-  + webpack的`watch`模式： webpack --watch. 如果启用，如果关联的文件有所改动，那webpack将自动重编译
-  + webpack-dev-server(WDS)：基于`watch`模式，并且还有更多的其他的好处
-    - running in-memory
-    - HMR
-  + webpack cli的参数  --env 是有效的，你可以更改配置文件来校验，将配置文件的`module.exports`改为一个方法，如下(具体可见`webpack.config.js`)：
-      ```js
-      module.exports = function(env) {
-        console.log('env', env);
-        return commonConfig;
-      }
-      ```
-      改完配置后，再运行 `build`、`start` 即可看出区别
+  + `HMR`有两种方式：  
+    - cli: webpack-dev-server --hot
+    - config: HotModuleReplacementPlugin  
 
+  其中两种方式不能同时使用，否则会引起Issue：[`Uncaught RangeError: Maximum call stack size exceeded`](https://github.com/webpack/webpack-dev-server/issues/87)
 
-  + webpack2 的`watch`模式暂时不支持监视配置文件的变化，但是一个完美的开发环境应该兼顾这一点，webpack声明将来会支持，暂时使用`nodemon`来做曲线救国(在package.json)：
-    ```js
-      {
-        ...
-        "start": "nodemon --watch webpack.config.js --exec \"webpack-dev-server --env development\""
-      }
-    ```
+  + 使用`HotModuleReplacementPlugin` 配置更为灵活，有很多高级的配置项可以帮助你搭建出一个完美的开发环境
+
 
 
 
@@ -32,4 +19,4 @@ starter for webpack2
 
 # Thanks to
 
-+ https://survivejs.com/webpack/developing/automatic-browser-refresh/
++ https://survivejs.com/webpack/developing/configuring-hmr/
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 
@@ -22,7 +23,71 @@ const commonConfig = {
   ]
 }
 
-module.exports = function(env) {
-  console.log('env', env);
+// production 模式下
+function prodConfig() {
   return commonConfig;
+}
+
+// development 模式下
+function devConfig() {
+  const config = {
+    devServer: {
+      // Enable history API fallback so HTML5 History API based
+      // routing works. This is a good default that comes
+      // in handy in more complicated setups.
+      historyApiFallback: true,
+
+      // Don't refresh if hot loading fails. If you want
+      // refresh behavior, set hot: true instead.
+      hotOnly: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env to allow customization.
+      //
+      // If you use Docker, Vagrant or Cloud9, set
+      // host: options.host || '0.0.0.0';
+      //
+      // 0.0.0.0 is available to all network devices
+      // unlike default `localhost`.
+      host: process.env.HOST, // Defaults to `localhost`
+      port: process.env.PORT, // Defaults to 8080
+
+      // Enable error/warning overlay
+      overlay: {
+        errors: true,
+        warnings: true,
+      },
+
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),  // --hot 与 HMR-plugin 二选一
+      new webpack.NamedModulesPlugin()  // module 不在是数字id
+    ]
+  }
+
+
+
+  return Object.assign(
+    {},
+    commonConfig,
+    config,
+    {
+      plugins: commonConfig.plugins.concat(config.plugins),
+    }
+  );
+}
+
+
+
+
+
+module.exports = function(env) {
+
+  if(env === "production") {
+    return prodConfig()
+  }
+
+  return devConfig();
 }
